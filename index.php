e4a41784a2f5def26865b5a53add36a0f69a078f
Humberto Filho <hr.soaresfilho@gmail.com>
2023-08-25 02:55:32 -0300

    Ajustes no layout responsivo

--- index.php
+++ index.php
@@ -24,9 +24,7 @@
             <a class="navbar-brand text-light" href="">
                 <i class="fa-solid fa-code fa-lg text-warning me-3"></i>dev<span class="fw-bold">Humberto</span>Filho
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -46,20 +44,16 @@
                 </ul>
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="https://github.com/HRSoaresFilho/" target="_blank"><i
-                                class="fa-brands fa-github fa-lg"></i></a>
+                        <a class="nav-link" href="https://github.com/HRSoaresFilho/" target="_blank"><i class="fa-brands fa-github fa-lg"></i></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="https://www.linkedin.com/in/hrsoaresfilho/" target="_blank"><i
-                                class="fa-brands fa-linkedin fa-lg"></i></a>
+                        <a class="nav-link" href="https://www.linkedin.com/in/hrsoaresfilho/" target="_blank"><i class="fa-brands fa-linkedin fa-lg"></i></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="mailto:hr.soaresfilho@gmail.com" target="_blank"><i
-                                class="fa-solid fa-envelope fa-lg"></i></a>
+                        <a class="nav-link" href="mailto:hr.soaresfilho@gmail.com" target="_blank"><i class="fa-solid fa-envelope fa-lg"></i></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="https://wa.me/5548988643890/" target="_blank"><i
-                                class="fa-brands fa-whatsapp fa-lg"></i></a>
+                        <a class="nav-link" href="https://wa.me/5548988643890/" target="_blank"><i class="fa-brands fa-whatsapp fa-lg"></i></a>
                     </li>
                 </ul>
             </div>
@@ -75,19 +69,17 @@
         <div class="container hero">
             <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
                 <div class="col-12 col-lg-6">
-                    <img src="dist/img/astronauta-hero.png" class="d-block mx-lg-auto img-fluid ms-auto me-auto"
-                        alt="Dev Humberto Filho" width="400" loading="lazy">
+                    <img src="dist/img/astronauta-hero.png" class="d-block mx-lg-auto img-fluid ms-auto me-auto" alt="Dev Humberto Filho" width="400" loading="lazy">
                 </div>
                 <div class="col-12 col-lg-6">
                     <h1 class="display-5">Olá!</h1>
                     <h1 class="display-5 lh-1 mb-3">Sou <span class="fw-bold">Humberto</span>,</h1>
                     <h2 class="fw-bold lh-1 mb-3 text-warning">desenvolvedor full stack.</h2>
-                    <p class="lead">Apaixonado por tecnologia, planilhas e finanças, com uma sólida formação 
+                    <p class="lead">Apaixonado por tecnologia, planilhas e finanças, com uma sólida formação
                         em Gestão de Tecnologia da Informação e pós-graduando em Análise e Desenvolvimento de Programas.
                     </p>
                     <div class="d-grid gap-2 d-md-flex justify-content-md-start mt-5">
-                        <a class="btn btn-warning btn-lg px-4 me-md-2 cta" href="https://wa.me/5548988643890/"
-                            target="_blank">
+                        <a class="btn btn-warning btn-lg px-4 me-md-2 cta" href="https://wa.me/5548988643890/" target="_blank">
                             Contato</a>
                         <a class="btn btn-outline-light btn-lg px-4 cta" href="#projects">Projetos</a>
                     </div>
@@ -96,65 +88,70 @@
         </div>
     </section>
     <section id="about">
-        <div class="container">
+        <div class="container mb-5">
             <h1 class="text-center mb-5 fw-bold">SOBRE MIM</h1>
-            <p>Sou um desenvolvedor Full Stack com mais de 5 anos de experiência, especializado na criação de soluções 
-                tecnológicas abrangentes. Minha jornada profissional abrange desde a construção de interfaces de usuário 
-                até a implementação de back-ends. Tenho proficiência em tecnologias modernas como HTML5, CSS3, JavaScript, 
+            <p>Sou um desenvolvedor Full Stack com mais de 5 anos de experiência, especializado na criação de soluções
+                tecnológicas abrangentes. Minha jornada profissional abrange desde a construção de interfaces de usuário
+                até a implementação de back-ends. Tenho proficiência em tecnologias modernas como HTML5, CSS3, JavaScript,
                 Vue.js e Bootstrap, combinadas com habilidades sólidas em PHP. </p>
-            <p>Minha versatilidade se estende além do desenvolvimento web, englobando a criação de sistemas eficientes através 
-                do Microsoft Excel. Automatizar tarefas repetitivas e desenvolver modelos complexos são aspectos do meu trabalho 
+            <p>Minha versatilidade se estende além do desenvolvimento web, englobando a criação de sistemas eficientes através
+                do Microsoft Excel. Automatizar tarefas repetitivas e desenvolver modelos complexos são aspectos do meu trabalho
                 que demonstram minha dedicação à otimização e à entrega de resultados tangíveis.</p>
-            <p>Minha paixão por finanças pessoais enriquece minha abordagem técnica. Minha compreensão das nuances financeiras 
-                me permite criar ferramentas de gerenciamento que capacitam os indivíduos a tomar decisões 
-                informadas. Fundir minhas habilidades técnicas com a expertise em finanças gera soluções 
+            <p>Minha paixão por finanças pessoais enriquece minha abordagem técnica. Minha compreensão das nuances financeiras
+                me permite criar ferramentas de gerenciamento que capacitam os indivíduos a tomar decisões
+                informadas. Fundir minhas habilidades técnicas com a expertise em finanças gera soluções
                 abrangentes e práticas.</p>
-            <p>Estou comprometido em enfrentar desafios progressivos que expandam meus horizontes e contribuam para projetos 
-                de impacto. Minha combinação única de habilidades técnicas diversificadas, visão financeira e competência 
-                interpessoal me capacita a abordar com sucesso os desafios em constante evolução no mundo do desenvolvimento 
+            <p>Estou comprometido em enfrentar desafios progressivos que expandam meus horizontes e contribuam para projetos
+                de impacto. Minha combinação única de habilidades técnicas diversificadas, visão financeira e competência
+                interpessoal me capacita a abordar com sucesso os desafios em constante evolução no mundo do desenvolvimento
                 de software e na esfera tecnológica.</p>
-            <div class="row row-cols-1 row-cols-md-3 g-4 mt-5">
-                <div class="col">
-                    <div class="card">
-                        <img src="dist/img/astronauta-card-1.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural
-                                lead-in
-                                to additional content. This content is a little bit longer.</p>
+        </div>
+        <div class="container-fluid bg-dark">
+            <div class="container py-5">
+                <div class="row row-cols-1 row-cols-md-3">
+                    <div class="col mb-4">
+                        <div class="card">
+                            <img src="dist/img/astronauta-card-1.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Foco na qualidade</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in
+                                    to additional content. This content is a little bit longer.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="dist/img/astronauta-card-2.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural
-                                lead-in
-                                to additional content. This content is a little bit longer.</p>
+                    <div class="col mb-4">
+                        <div class="card">
+                            <img src="dist/img/astronauta-card-2.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Estou aqui para ajudar</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in
+                                    to additional content. This content is a little bit longer.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                        <img src="dist/img/astronauta-card-3.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural
-                                lead-in
-                                to additional content.</p>
+                    <div class="col">
+                        <div class="card">
+                            <img src="dist/img/astronauta-card-3.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Cliente satisfeito = Dev Satisfeito</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in
+                                    to additional content.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </section>
     <section id="projects">
         <div class="container mb-5">
             <h1 class="text-center mb-5 fw-bold">PROJETOS</h1>
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                <div class="col">
+                <div class="col my-2">
                     <div class="card shadow-sm">
                         <img src="dist/img/projeto-portifolio.png" alt="">
                         <div class="card-body">
@@ -167,16 +164,14 @@
                                     </small>
                                 </div>
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i
-                                            class="fa-brands fa-github"></i></button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i
-                                            class="fa-solid fa-up-right-from-square"></i></button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-brands fa-github"></i></button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-up-right-from-square"></i></button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col my-2">
                     <div class="card shadow-sm">
                         <img src="dist/img/projeto-granah.png" alt="">
                         <div class="card-body">
@@ -189,16 +184,14 @@
                                     </small>
                                 </div>
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i
-                                            class="fa-brands fa-github"></i></button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i
-                                            class="fa-solid fa-up-right-from-square"></i></button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-brands fa-github"></i></button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-up-right-from-square"></i></button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col my-2">
                     <div class="card shadow-sm">
                         <img src="dist/img/projeto-simulador-1-scprev.png" alt="">
                         <div class="card-body">
@@ -211,10 +204,8 @@
                                     </small>
                                 </div>
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i
-                                            class="fa-brands fa-github"></i></button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i
-                                            class="fa-solid fa-up-right-from-square"></i></button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-brands fa-github"></i></button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-up-right-from-square"></i></button>
                                 </div>
                             </div>
                         </div>
@@ -234,10 +225,8 @@
                                     </small>
                                 </div>
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i
-                                            class="fa-brands fa-github"></i></button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i
-                                            class="fa-solid fa-up-right-from-square"></i></button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-brands fa-github"></i></button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-up-right-from-square"></i></button>
                                 </div>
                             </div>
                         </div>
@@ -249,15 +238,14 @@
 
     <section id="contact">
         <div class="container">
-            <h1 class="text-center fw-bold">CONTATO</h1>
-            <div class="row">
-                <div class="col-6"><img src="dist/img/astronauta-contato.jpg" width="400" alt=""></div>
-                <div class="col-6">
+            <h1 class="text-center mb-5 fw-bold">CONTATO</h1>
+            <div class="row row-cols-1 row-cols-md-2">
+                <div class="col mb-5 text-center"><img id="img-contato" src="dist/img/astronauta-contato.jpg" width="300" alt=""></div>
+                <div class="col">
                     <form action="">
                         <div class="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">E-mail</label>
-                            <input type="email" class="form-control" id="exampleFormControlInput1"
-                                placeholder="name@example.com">
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
                         </div>
                         <div class="mb-3">
                             <label for="exampleFormControlTextarea1" class="form-label">Mensagem</label>
